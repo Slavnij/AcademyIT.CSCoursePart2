f1bf785edf1def5d6fa6799bee437884ca6ee968
Evgeny Ipatov <101337644+Slavnij@users.noreply.github.com>
2022-11-06 14:09:08 +0300

    Delete 20487 Mod 3 Lab1 directory

--- 20487 Mod 3 Lab1/Starter/DAL/Models/Traveler.cs
+++
@@ -1,9 +0,0 @@
-namespace DAL.Models
-{
-    public class Traveler
-    {
-        public int TravelerId { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-    }
-}
